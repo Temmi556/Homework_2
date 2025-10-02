b5c67653f2696ca303ce7b0380e277446bde8233
Temmi556 <temmi556@mail.ru>
2025-10-02 21:56:58 +0300

    Initial Homework_2.c

--- Homework_2.c
+++ Homework_2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    int account,total_account;
-    printf("Enter account number: ");
-    scanf("%d", &account);
-    total_account = account * 4;
-    printf("Tip amount: %.0f\n", total_account * 0.15);
+    double Q,M,N;
+    printf("Введите объем в квартах: ");
+    scanf("%lf",&Q);
+    M = Q * 950;
+    N = M / 3.0e-23;
+    printf("Количество молекул в %.2lf квартах: %.2e\n",Q,N);
 }
